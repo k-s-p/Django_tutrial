0c220b571c732ef7e67175e3d4a2d5b357c0f35e
k-s-p <haruichiban0920@gmail.com>
2021-07-13 23:05:17 +0900

    SECRET_KEY変更

--- cycle1/mysite/mysite/settings.py
+++ cycle1/mysite/mysite/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-al7xg!67e^x^a+&rww9ppjafy=erywmrlfqf_=#b7#%9-u5zuq'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
